fc50946a701aed3cd9f34d8cfd553702cb343aa7
Izumi Takayuki <r8quattrospider@gmail.com>
2026-09-03 18:53:58 +0900

    style: Adjustment Design

--- src/components/LoginDisplay.tsx
+++ src/components/LoginDisplay.tsx
@@ -4,9 +4,16 @@ import { useLogin } from "../contexts/LoginContext";
 const LoginDisplay = () => {
   const { name, isLogin } = useLogin();
   return (
-    <div>
-      <p>ユーザー名: {isLogin ? name : ""}</p>
-      <p>ログイン: {isLogin ? "ログイン" : "ログアウト"} </p>
+    <div className="p-10 border rounded-2xl w-1/2 mx-auto my-5 text-white text-2xl">
+      <p className="pb-5">ユーザー名: {isLogin ? name : ""}</p>
+      <p>
+        ログイン:{" "}
+        {isLogin ? (
+          <span className="text-green-500 font-bold">ログイン</span>
+        ) : (
+          <span className="text-red-500 font-bold">ログアウト</span>
+        )}{" "}
+      </p>
     </div>
   );
 };
